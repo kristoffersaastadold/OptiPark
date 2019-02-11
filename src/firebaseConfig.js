7f7bdac7dada71cb96a081fefbaaf5865a91703a
kristoffersaastad <35805000+kristoffersaastad@users.noreply.github.com>
2019-02-10 23:49:16 -0800

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyCz2Lbfoxu59bbPKz5GaWNczSvq_I0Uh_E",
-    authDomain: "optipark-5dfe8.firebaseapp.com",
-    databaseURL: "https://optipark-5dfe8.firebaseio.com",
-    projectId: "optipark-5dfe8",
-    storageBucket: "optipark-5dfe8.appspot.com",
-    messagingSenderId: "887530230166"
-}
